39ff663b678bacff116a2529f613f2febac418ba
tanvirheer <Tony@eduroam-169-233-254-202.ucsc.edu>
2017-01-22 01:53:38 -0800

    New Code

--- Hackathon/app/src/main/java/com/hexd/hexd/carpenter.java
+++ Hackathon/app/src/main/java/com/hexd/hexd/carpenter.java
@@ -1,6 +1,7 @@
 package com.hexd.hexd;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,7 +10,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class carpenter extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,56 +30,46 @@ public class carpenter extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-
-        Button btn8 = (Button)findViewById(R.id.button8);
-        Button btn9 = (Button)findViewById(R.id.button9);
-        Button btn10 = (Button)findViewById(R.id.button10);
-        Button btn11 = (Button)findViewById(R.id.button11);
-        Button btn12 = (Button)findViewById(R.id.button12);
-        Button btn13 = (Button)findViewById(R.id.button13);
+        Button btn8 = (Button) findViewById(R.id.button8);
+        Button btn9 = (Button) findViewById(R.id.button9);
+        Button btn10 = (Button) findViewById(R.id.button10);
+        Button btn11 = (Button) findViewById(R.id.button11);
+        Button btn12 = (Button) findViewById(R.id.button12);
+        Button btn13 = (Button) findViewById(R.id.button13);
 
         View.OnClickListener onClick = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Button btn = (Button) v;
 
-                switch (btn.getId()){
+                switch (btn.getId()) {
                     case R.id.button8:
-                        Intent app = new Intent(carpenter.this,StatisticsMap.class);
+                        Intent app = new Intent(carpenter.this, StatisticsMap.class);
                         startActivity(app);
                         break;
 
                     case R.id.button9:
-                        Intent app1 = new Intent(carpenter.this,StatisticsMap.class);
+                        Intent app1 = new Intent(carpenter.this, StatisticsMap.class);
                         startActivity(app1);
                         break;
 
                     case R.id.button10:
-                        Intent app2 = new Intent(carpenter.this,StatisticsMap.class);
+                        Intent app2 = new Intent(carpenter.this, StatisticsMap.class);
                         startActivity(app2);
                         break;
 
                     case R.id.button11:
-                        Intent app3 = new Intent(carpenter.this,StatisticsMap.class);
+                        Intent app3 = new Intent(carpenter.this, StatisticsMap.class);
                         startActivity(app3);
                         break;
 
                     case R.id.button12:
-                        Intent app4 = new Intent(carpenter.this,StatisticsMap.class);
+                        Intent app4 = new Intent(carpenter.this, StatisticsMap.class);
                         startActivity(app4);
                         break;
 
                     case R.id.button13:
-                        Intent app5 = new Intent(carpenter.this,otherpage.class);
+                        Intent app5 = new Intent(carpenter.this, otherpage.class);
                         startActivity(app5);
                         break;
 
@@ -82,6 +84,44 @@ public class carpenter extends AppCompatActivity {
         btn11.setOnClickListener(onClick);
         btn12.setOnClickListener(onClick);
         btn13.setOnClickListener(onClick);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("carpenter Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
 }
--- Hackathon/app/src/main/java/com/hexd/hexd/sixthscreen.java
+++ Hackathon/app/src/main/java/com/hexd/hexd/sixthscreen.java
@@ -18,15 +18,6 @@ public class sixthscreen extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
 
         Button btn8 = (Button)findViewById(R.id.button8);
         Button btn9 = (Button)findViewById(R.id.button9);
